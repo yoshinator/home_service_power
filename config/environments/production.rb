a4ffa61ee041a955ab19c2a2e1f8dc6ab23c3044
yoshinator <yoanante@gmail.com>
2019-11-16 23:36:51 -0500

    testing sendgrid integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
-    port: 465,
+    port: 587,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['EMAIL'],
